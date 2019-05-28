d1ecaf97e0a12b3088a36e0fe4e17f0f0df66307
Monica Gondolini <monica.gondolini@studio.unibo.it>
2019-05-28 12:50:12 +0200

    Implemented Reduce for Job2.

--- MapReduce/src/main/java/job2/Job2.java
+++ MapReduce/src/main/java/job2/Job2.java
@@ -12,9 +12,7 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.*;
 
 
 public class Job2 {
@@ -38,28 +36,43 @@ public class Job2 {
 					}
 				}
 			}else return;
-		}
+			}
 	}
 
 	public static class TagReducer extends Reducer<Text, Text, Text, Text> {
 
 		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 
-//			HashMap<String, Integer> tagMap = new HashMap<>();
-//
-//			String[] tags;
-//			for(Text tagList: values){
-//				tags = tagList.toString().split("|");
-//				for(String tag: tags){
-//					incrementTagOccurrence(tagMap, tag);
-//				}
-//			}
+			HashMap<String, Integer> tagMap = new HashMap<>();
 
-			context.write(new Text("category: "+key), new Text("tags: "+values.iterator().next()));
+			for(Text tag: values) incrementTagOccurrence(tagMap, tag.toString());
+
+			Map<String, Integer> orderedTags = sortByValue(tagMap);
+
+			List<Map.Entry<String,Integer>> results = new ArrayList<>(orderedTags.entrySet());
+
+			context.write(new Text("\ncategory: "+key), new Text("tags: "+ results.subList(0, 10)));
 		}
 	}
 
+	private static <K, V> Map<K, V> sortByValue(Map<K, V> map) {
+		List<Map.Entry<K, V>> list = new LinkedList<>(map.entrySet());
+		Collections.sort(list, new Comparator<Object>() {
+			@SuppressWarnings("unchecked")
+			public int compare(Object o1, Object o2) {
+				return ((Comparable<V>) ((Map.Entry<K, V>) (o2)).getValue()).compareTo(((Map.Entry<K, V>) (o1)).getValue());
+			}
+		});
+		Map<K, V> result = new LinkedHashMap<>();
+		for (Map.Entry<K, V> entry : list) {
+			result.put(entry.getKey(), entry.getValue());
+		}
+		return result;
+	}
+
 	private static <K> void incrementTagOccurrence(Map<K,Integer> map, K key){
+//		map.merge(key, 1, (a, b) -> a + b);
+
 		Integer count = map.get(key);
 		if(count == null){
 			map.put(key, 1);
@@ -67,6 +80,7 @@ public class Job2 {
 			map.put(key, count+1);
 		}
 	}
+
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
 		Configuration conf = new Configuration();
