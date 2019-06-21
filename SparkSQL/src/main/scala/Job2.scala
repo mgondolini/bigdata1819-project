0a667ae2a61acf42282c60e40d8b2abb3b4cde56
Monica Gondolini <monica.gondolini@studio.unibo.it>
2019-06-21 16:20:58 +0200

    Implemented Job2 with Spark Sql queries.

--- SparkSQL/src/main/scala/Job2.scala
+++ SparkSQL/src/main/scala/Job2.scala
@@ -1,7 +1,6 @@
 // Local init
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.{DataFrame, Row}
 
 val sc = new SparkContext(new SparkConf().setAppName("Youtube Trending Videos"))
 
@@ -9,71 +8,66 @@ val sc = new SparkContext(new SparkConf().setAppName("Youtube Trending Videos"))
 val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 
 // File path
-val categoryJsonFilePath: String = "dataset/US_category_id_flat.json"
-val caVideosCsvFilePath: String = "dataset/CAvideos.csv"
-val gbVideosCsvFilePath: String = "dataset/GBvideos.csv"
-val usVideosCsvFilePath: String = "dataset/USvideos.csv"
-
+val categoryJsonFilePath: String = "project/dataset/US_category_id_flat.json"
+val caVideosCsvFilePath: String = "project/dataset/cleaned/CAvideos.csv"
+val gbVideosCsvFilePath: String = "project/dataset/cleaned/GBvideos.csv"
+val usVideosCsvFilePath: String = "project/dataset/cleaned/USvideos.csv"
 
 //Import data from JSON file to DataFrame
 val categoryNames = sqlContext.read.json(categoryJsonFilePath)
 
-// SPARK SHELL 1
-//Load to dfa CSV without a defined schema
-//import org.apache.spark.sql.types.{StructType,StructField,StringType}
-//val videos = sc.textFile(caVideosCsvFilePath)
-//val schemaString = "video_id trending_date title channel_title category_id publish_time tags views likes dislikes comment_count thumbnail_link comments_disabled ratings_disabled video_error_or_removed description"
-//val schema = StructType(schemaString.split(" ").map(fieldName => StructField(fieldName, StringType, true)))
-//val rowRDD = videos.map(_.split(",")).map(e => Row(e(0), e(1), e(2), e(3), e(4), e(5), e(6), e(7), e(8), e(9), e(10), e(11), e(12), e(13), e(14), e(15)))
-//val videosDF = sqlContext.createDataFrame(rowRDD, schema)
-
 // SPARK SHELL 2
-
 val CaDF = sqlContext.read.format("csv").option("delimiter", ",").option("header", "true").option("mode", "DROPMALFORMED").load(caVideosCsvFilePath)
 
 val GbDF = sqlContext.read.format("csv").option("delimiter", ",").option("header", "true").option("mode", "DROPMALFORMED").load(gbVideosCsvFilePath)
 
 val UsDF = sqlContext.read.format("csv").option("delimiter", ",").option("header", "true").option("mode", "DROPMALFORMED").load(usVideosCsvFilePath)
 
-
-//da controllare HEADER
+// UNION
 val trendingVideosUnionDF = CaDF.union(GbDF).union(UsDF)
 
-//val trendingVideosJoinCategoryDF = trendingVideosUnionDF.join(categoryNames, trendingVideosUnionDF("category_id")===categoryNames("id"), "left").drop("category_id").drop("id")
-
+// JOIN
 val broadcastVideosJoin = trendingVideosUnionDF.join(broadcast(categoryNames.withColumnRenamed("id", "category_id")), "category_id")
+broadcastVideosJoin.registerTempTable("trendingVideosTmp")
 
-// Nel caso volessimo la colonna Category nella vecchia posisizione di category_id
-val reorderedColumnNames: Array[String] = Array("video_id", "trending_date", "title", "channel_title", "category_id", "category", "publish_time", "tags", "views", "likes", "dislikes", "comment_count", "thumbnail_link", "comments_disabled", "ratings_disabled", "video_error_or_removed", "description")
-val trendingVideosDFCategoryOrdered = broadcastVideosJoin.select(reorderedColumnNames.head, reorderedColumnNames.tail: _*)
+// Dataframe composed by category and tags columns
+val categoryTags = sqlContext.sql("select category, tags from trendingVideosTmp")
+categoryTags.show()
 
-// Seleziona le colonne category e tags
-val categoryTags = trendingVideosDFCategoryOrdered.select("category_id", "category", "tags")
-//Si può fare anche così
-//trendingVideosDFCategoryOrdered.registerTempTable("trendingVideos")
-//val categoryTags2 = sqlContext.sql("select category, tags from trendingVideos")
+// Explode tag list
+val categoryTagsExploded = categoryTags.withColumn("tags", explode(split($"tags", "(\\|)")))
+categoryTagsExploded.registerTempTable("categoryTagsExplodedTmp")
 
-// Raggruppa i tag per categoria
-val groupedByCategory = categoryTags.groupBy(col("category_id"), col("category")).agg(collect_list(col("tags")) as "tags").withColumn("tags", split(concat_ws(",", col("tags")), "(\\|)"))
+// Count the same tags for each category, create a column count_tag
+val tagsCount = sqlContext.sql("select category, tags, COUNT(*) from categoryTagsExplodedTmp group by category, tags").withColumnRenamed("count(1)", "count_tag")
+tagsCount.show()
+tagsCount.registerTempTable("tagsCountTmp")
+
+// Order tags by category and descending count value
+val orderCategoryTags = sqlContext.sql("select * from tagsCountTmp order by category, count_tag desc")
+orderCategoryTags.registerTempTable("orderCategoryTags")
+
+//TODO forse non serve orderTags e si può fare tutto con questa query
+val top10query = """ select category, tags, count_tag
+				from (select *, row_number() over (partition by category order by category, count_tag desc) as top_n_rows
+				      from orderCategoryTags
+				     ) a
+				where top_n_rows <= 10
+				order by a.category, a.count_tag desc """
+
+// Select top 10 rows for each category
+val top10CategoryTags =  sqlContext.sql(top10query)
+top10CategoryTags.show()
+
+import org.apache.spark.sql.functions.struct
+
+// Collapse tags and count_tags column values in one column "tags:count"
+val collapsedTagsCount = top10CategoryTags.withColumn("tags:count", struct(tagsNumber("tags"), tagsNumber("count_tag"))).drop("tags").drop("count_tag")
+collapsedTagsCount.show()
+
+// Group Dataframe by category, creating a list of tags:count in column top10_tags
+val groupedByCategory = collapsedTagsCount.groupBy("category").agg(collect_list(col("tags:count")) as "top10_tags")
 groupedByCategory.show()
 
-// Converte il formato della colonna tags da Array[String] a String
-val groupedByCategoryString = groupedByCategory.as[(String, String, Array[String])].map { case (id, category, tags) => (id, category, tags.mkString(",")) }.toDF("category_id","category", "tags")
-groupedByCategoryString.show()
-
-// Stampa i tag per categoria
-groupedByCategory.select("category_id","category","tags").take(1).foreach(println)
-
-
-
-
-
-
-
-
-// ------ COSE UTILI -------
-// Trasformare il DF in RDD
-val rows = groupedByCategory.rdd
-// Prendere un elemento della riga
-rows.map(row => row.get(1).asInstanceOf[String])
-
+// Stampa tutto
+groupedByCategory.collect().foreach(println)
